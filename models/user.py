416f4478f4f49a94b5789fa890d9e84cb3371d37
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-28 19:45:28 -0400

    rearranged import order

--- models/user.py
+++ models/user.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import Base
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 
 
-
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
