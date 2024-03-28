cb39493909f3a9a3e8d1d612bec6f7a6cb4423fe
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-28 19:17:42 -0400

    deleted unnecesary nullable

--- models/user.py
+++ models/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
-from models.base_model import BaseModel, Base
+
 
 
 class User(BaseModel, Base):
@@ -10,5 +12,5 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
