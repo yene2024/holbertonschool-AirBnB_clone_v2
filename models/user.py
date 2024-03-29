1c7d07fd5c02878bbad0e46d1e8d8a46e890802b
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-28 23:40:17 -0400

    modified user and place file

--- models/user.py
+++ models/user.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column
-from sqlalchemy import String
+import models
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = db.Column(String(128), nullable=False)
+    password = db.Column(String(128), nullable=False)
+    first_name = db.Column(String(128))
+    last_name = db.Column(String(128))
     
     places = relationship("Place", backref="user", cascade="delete")
