17b8e0caf651808e09b3786d2c38fd9816330af3
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-29 11:26:23 -0400

    Add relationship between Place and Review models/fixed style

--- models/user.py
+++ models/user.py
@@ -12,5 +12,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    
-    places = relationship("Place", backref="user", cascade="all, delete-orphan")
+
+    places = relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
