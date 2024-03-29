885b34144d9c9d846dc5c1fa4f0650fd1ba241b5
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-28 22:13:49 -0400

    modified place import

--- models/place.py
+++ models/place.py
@@ -2,11 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy import Integer
-from sqlalchemy import Float
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
 class Place(BaseModel, Base):
