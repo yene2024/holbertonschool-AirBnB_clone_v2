de6c4976aec2a4da56e08cae5b3f563fa4cff083
yene2024 <yeneishla1524@gmail.com>
2024-03-29 22:09:42 -0400

    fixing imports in amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Table, ForeingKey
+from sqlalchemy.orm import relationship
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
@@ -15,5 +15,5 @@ class Amenity(BaseModel, Base):
     """This represents amenities"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Amenity', secondary=place_amenities,
+    place_amenities = relationship('Amenity', secondary=place_amenity,
                                    backref="amenities")
