9b8bdee098723ad8fd0955432d4bb2311c2d07d5
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-29 23:37:12 -0400

    fixed syntax and modified code

--- models/review.py
+++ models/review.py
@@ -7,6 +7,12 @@ from sqlalchemy import Column, String, ForeignKey
 class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    if getenv(HBNB_TYPE_STORAGE) =='db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
