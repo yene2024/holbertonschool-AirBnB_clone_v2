95766085b77601cc7b2beebe2cb5d6b4ec582432
yene2024 <yeneishla1524@gmail.com>
2024-04-11 12:11:14 -0400

    Fixing the code.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,17 @@
-#!user/bin/python3
+#!/usr/bin/python3
+""" This module starts a Flask web application """""
 from flask import Flask
-"""This script starts a Flask web application"""
+"""Flask class and render_template method"""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+""" App route section"""
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/')
+def hello():
     return 'Hello HBNB!'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
