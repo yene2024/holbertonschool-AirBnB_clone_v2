01f1691b961cfa13b92d54575dc607c1af7251ae
yene2024 <yeneishla1524@gmail.com>
2024-04-11 12:13:56 -0400

    Fixing the code.

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,24 @@
-#!user/bin/python3
+#!/usr/bin/python3
+""" This module starts a Flask web application """""
 from flask import Flask
-"""This script starts a Flask web application"""
+""" Flask class and render_template method"""""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+""" App route section
+    Add a second route /hbnb:
+    display “HBNB”"""""
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/')
+def hello():
     return 'Hello HBNB!'
-@app.route('/hbnb', strict_slashes=False)
+
+
+@app.route('/hbnb')
 def hbnb():
     return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
