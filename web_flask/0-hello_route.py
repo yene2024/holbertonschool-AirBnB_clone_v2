e7736b1c47a7bbad8aa60c98a10853bccfd5e241
yene2024 <yeneishla1524@gmail.com>
2024-04-11 13:08:07 -0400

    added strict slashes to the decorator.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,12 @@
 from flask import Flask
 """Flask class and render_template method"""
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 """ App route section"""
 
-
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
