b44ff899105291cde0c1e48852d36d77f0c9579c
yene2024 <yeneishla1524@gmail.com>
2024-04-11 14:59:35 -0400

    Fixing code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+""" Flask class and render_template method"""""
 app = Flask(__name__)
-
-
-@app.route('/cities_by_states')
-def states_list():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown(self):
     storage.close()
 
 
+@app.route('/cities_by_states')
+def cities_by_states():
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
