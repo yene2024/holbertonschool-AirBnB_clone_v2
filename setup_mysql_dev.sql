784d2a85f3e124f8b3c73540ad1aff6ed35fd627
yene2024 <yeneishla1524@gmail.com>
2024-04-11 12:02:27 -0400

    Changing the Schemas

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,18 +1,8 @@
--- Script that prepares a MySQL server for AirBnB Console v2
--- This script will create a database and a user for the AirBnB project
-
--- Create the database
+-- Create dev database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the user if not exists
--- The user will be created with the password 'hbnb_dev_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on the database to the user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-Flush privileges;
-
--- Grant select privileges on performance_schema to the user
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-Flush privileges;
+
 
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,17 +1,6 @@
--- Script that prepares a MySQL server for AirBnB Console v2
--- This script will create a database and a user for the AirBnB project
--- Create the database
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create the user if not exists
--- Create user in localhost identified by 'hbnb_test_pwd'
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant all privileges on the database to the user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-Flush privileges;
-
--- Grant select privileges on performance_schema to the user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-Flush privileges;
-
+-- Create dev database and user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
