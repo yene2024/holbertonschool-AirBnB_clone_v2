7994889a33ac38b78dd4d24006f1493194397240
yene2024 <yeneishla1524@gmail.com>
2024-04-11 15:08:57 -0400

    running and valid

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,16 @@
--- Create dev database and user
+-- Prepare MySQL server
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
+-- Grant privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
+-- Grant SELECT privileges
+GRANT
+SELECT
+    ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
