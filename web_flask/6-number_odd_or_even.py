9e9df5d5a1b3617097a45015c9061e580220b503
yene2024 <yeneishla1524@gmail.com>
2024-04-11 12:45:21 -0400

    Fixing the html

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,62 +1,58 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
-from flask import Flask, render_template
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
+""" Flask class and render_template method"""""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+""" App route section
+    Add a second route /hbnb:
+    display “HBNB”
+
+    Add a third route /c/<text>:
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space ) "
+
+    Add a fourth route /python/(<text>):
+    display “Python ”, followed by the value of the text variable"""""
 
 
 @app.route('/')
 def hello():
-    """Return string when route queried
-    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Return string when route queried
-    """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text
-    """
-    return 'C ' + text.replace('_', ' ')
+def c(text):
+    return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python/')
+@app.route('/python', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
-    return 'Python ' + text.replace('_', ' ')
+def python(text='is cool'):
+    return f"Python {text.replace('_', ' ')}"
 
 
 @app.route('/number/<int:n>')
-def number(n=None):
-    """Allow request if path variable is a valid integer
-    """
-    return str(n) + ' is a number'
+def number(n):
+    return f"{n} is a number"
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """Retrieve template for request
-    """
-    path = '5-number.html'
-    return render_template(path, n=n)
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even(n):
-    """Render template based on conditional
-    """
-    path = '6-number_odd_or_even.html'
-    return render_template(path, n=n)
+def number_odd_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
