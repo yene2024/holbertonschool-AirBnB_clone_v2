7319f04dfa1a02ff254409bd7aedadc13b17f583
yene2024 <yeneishla1524@gmail.com>
2024-04-12 19:30:27 -0400

    Fixing code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application
-"""
+"""Start web application with two routings"""
 
-from flask import Flask, render_template
 from models import storage
 from models.state import State
-from sqlalchemy.orm import sessionmaker
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/states_list')
+def states_list():
+    """Render template with states"""
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+def app_teardown(arg=None):
+    """Clean-up session"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with a list of all State objects"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
