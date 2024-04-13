eb66bae2a3755e016a89162de560bbe90c58232e
yene2024 <yeneishla1524@gmail.com>
2024-04-12 21:45:10 -0400

    Writing a script that starts a Flask web application

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-"""Start web application with two routings"""
-
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-
+""" Flask class and render_template method"""""
 app = Flask(__name__)
-
-
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """Render template with states"""
-    path = '9-states.html'
-    states = storage.all(State)
-    return render_template(path, states=states, id=id)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session"""
+def teardown(self):
     storage.close()
 
 
+@app.route('/states')
+def states():
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+# @app.route('/states/<int:id>')
+# def states_id(id):
+#     states = storage.all(id)
+#     return render_template('9-states.html', id=id)
+
+@app.route('/states/<id>')
+def states_id(id):
+    states = storage.all(State).values()
+    state = next((state for state in states if state.id == id), None)
+    if state is not None:
+        cities = sorted(state.cities, key=lambda city: city.name)
+    else:
+        cities = []
+    return render_template('9-states.html', state=state, cities=cities)
+
+
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
