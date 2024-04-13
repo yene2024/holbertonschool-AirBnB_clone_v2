9a6df5c6de8f839689142d03f0298e3f3eafabf0
yene2024 <yeneishla1524@gmail.com>
2024-04-12 21:53:31 -0400

    Writing a script that starts a Flask web application

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application """""
-from flask import Flask
-from flask import render_template
+"""Start web application with two routings
+"""
+
 from models import storage
 from models.state import State
-""" Flask class and render_template method"""""
+from flask import Flask, render_template
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.teardown_appcontext
-def teardown(self):
-    storage.close()
 
 
 @app.route('/cities_by_states')
 def cities_by_states():
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
     states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """Clean-up session
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
