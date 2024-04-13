4c60fd7a0b9bccf4936d66fe0502983073201f83
yene2024 <yeneishla1524@gmail.com>
2024-04-12 22:20:48 -0400

    Fixing the errors

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
-
+""" Flask class and render_template method"""""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
 
 
 @app.route('/cities_by_states')
 def cities_by_states():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
     states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
-
-
-@app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
-    storage.close()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -26,3 +26,43 @@ def app_teardown(arg=None):
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
+#!/usr/bin/python3
+""" This module starts a Flask web application """""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+""" Flask class and render_template method"""""
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+
+@app.route('/states')
+def states():
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+# @app.route('/states/<int:id>')
+# def states_id(id):
+#     states = storage.all(id)
+#     return render_template('9-states.html', id=id)
+
+@app.route('/states/<id>')
+def states_id(id):
+    states = storage.all(State).values()
+    state = next((state for state in states if state.id == id), None)
+    if state is not None:
+        cities = sorted(state.cities, key=lambda city: city.name)
+    else:
+        cities = []
+    return render_template('9-states.html', state=state, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
